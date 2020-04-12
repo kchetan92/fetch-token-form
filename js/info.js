0d520ca61fd38d2d166d2547dd26243875834f4d
Chetan Keshav <ckeshav@illumina.com>
2020-04-12 15:11:02 -0700

    ok test added

--- js/info.js
+++ js/info.js
@@ -1,10 +1,19 @@
 var TARGET_URL =
   "http://ec2-13-235-229-219.ap-south-1.compute.amazonaws.com:6868/unlockdebugaccess";
 
+function checkCredentialError(res) {
+  if (res && "ok" in res && res["ok"]) {
+    return res;
+  } else {
+    throw Error("Incorrect response");
+  }
+}
+
 (function () {
   var tokenDisplay = document.getElementById("token");
   var container = document.getElementById("token-result");
   var sendButton = document.getElementById("send");
+  var tokenSent = document.getElementById("token-sent");
 
   var token = window.localStorage.getItem("token");
   if (token) {
@@ -19,8 +28,18 @@ var TARGET_URL =
       method: "GET",
       headers: {
         deviceID: window.localStorage.getItem("deviceID"),
-        token: window.localStorage.getItem("token"),
+        Authorization: window.localStorage.getItem("token"),
       },
+    }).then((res) => {
+      if (res.status === 200) {
+        tokenSent.innerText = "OK";
+      } else if (res.status === 401) {
+        tokenSent.innerText = "Unauthorized";
+      } else {
+        tokenSent.innerText = `Unknown error. Error code: ${res.status}`;
+      }
+
+      tokenSent.classList.add("show");
     });
   });
 })();
